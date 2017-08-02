e63f291ecc745be2725add19685487b5d5d3fdd0
twlgogo <twlgogo@163.com>
2017-08-02 17:36:31 +0800

    8.2实验室刷题提交

--- RangeAdditionII.java
+++ RangeAdditionII.java
@@ -3,6 +3,8 @@
  * User: ttwl
  * Date: 2017/8/2
  * Time: 17:12
+ *知识点：怎么判断一个二维数组为空，同理判断一维数组为空只需要判断
+ * 1.数组未初始化 2.数组已经初始化但是长度为0
  */
 public class RangeAdditionII {
     public static int maxCount(int m, int n, int[][] ops) {
