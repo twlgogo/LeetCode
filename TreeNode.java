b72fe883544e0634c9b8e33b7cc68159da617817
twlgogo <twlgogo@163.com>
2017-08-05 21:36:46 +0800

    增加工具类 二叉树

---
+++ TreeNode.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Intellij
+ * User: ttwl
+ * Date: 2017/8/5
+ * Time: 21:35
+ */
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
