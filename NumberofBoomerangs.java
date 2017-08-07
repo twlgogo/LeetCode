eda52ee7456485e5a42de0a9696bb196df17fdad
twlgogo <twlgogo@163.com>
2017-08-07 16:34:08 +0800

    8/7刷题总结 达到60道题，庆祝一下

---
+++ NumberofBoomerangs.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Intellij
+ * User: ttwl
+ * Date: 2017/8/7
+ * Time: 16:00
+ */
+public class NumberofBoomerangs {
+    public int numberOfBoomerangs(int[][] points) {
+        int result =0;
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i <points.length ; i++) {
+            for (int j = 0; j <points.length ; j++) {
+                if (i != j){
+                    int currDis = computingDis(points[i],points[j]);
+                    map.put(currDis,map.getOrDefault(currDis,0)+1);
+                }
+            }
+            for (int value:map.values()) {
+                result += value*(value-1);
+            }
+            map.clear();
+        }
+        return result;
+    }
+    private int computingDis(int[] i,int[] j){
+        int a = i[0] - j[0];
+        int b = i[1] - j[1];
+        return a*a+b*b;
+    }
+}
